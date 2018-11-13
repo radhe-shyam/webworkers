3b0867473136c5056a1fac5d6e4e0666d152ef57
radhe-evc <sam.shyam@evclabs.com>
2018-11-13 11:06:07 -0500

    :alien: init

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function () {
+    document.getElementById('fibSeries').addEventListener('submit', function (e) {
+        try {
+            var seriesLength = Number(document.getElementById('seriesLength').value);
+            if (!seriesLength && seriesLength < 1) {
+                alert('Series length must be a positive number.');
+            } else {
+                var seriesBoard = document.getElementById('seriesBoard');
+                seriesBoard.innerHTML = seriesLength;
+                if (typeof Worker == undefined) {
+                    seriesBoard.innerHTML = 'Your browser doesn\'t support workers';
+                } else {
+                    var w = new Worker(window.URL.createObjectURL(new Blob([document.getElementById('fib-worker').textContent], { type: "text/javascript" })));
+                    w.postMessage({
+                        seriesLength: seriesLength
+                    });
+                }
+            }
+            e.preventDefault();
+        } catch (error) {
+            console.log(error);
+            document.body.innerHTML = '<h1>Its not you, its US :\'(</h1>';
+            e.preventDefault();
+        }
+    });
+
+})
